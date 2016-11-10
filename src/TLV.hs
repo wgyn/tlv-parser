412d3d64b55e3f63a901611b5ff65732fd5d5716
Ryan Wang <wgyn888@gmail.com>
2016-11-09 21:31:54 -0800

    Handle tag parsing

--- src/TLV.hs
+++ src/TLV.hs
@@ -1,7 +1,31 @@
+-- An implementation of BER-TLV parsing according to EMV Book 3, Annex B
 module TLV ( parseTag ) where
 
 import Data.Bits
+import Data.Char (intToDigit)
 import Data.Word
+import Numeric (showHex, showIntAtBase)
+import Text.Printf (printf)
 
-parseTag :: [Word8] -> ([Word8], [Word8])
-parseTag raw = ([head raw], tail raw)
+type Byte = Word8
+type Bytes = [Byte]
+
+-- | The tag field (T) consists of one or more consecutive bytes. It
+-- indicates a class, a type, and a number. The tag field field of the data
+-- object in this specification is coded on one or two bytes.
+parseTag :: Bytes -> (Bytes, Bytes)
+parseTag [] = ([], [])
+parseTag (x:xs) | (x .&. 0x1F) == 0x1f = parseTagSubsequentBytes [x] xs
+                | otherwise = ([x], xs)
+
+parseTagSubsequentBytes :: Bytes -> Bytes -> (Bytes, Bytes)
+parseTagSubsequentBytes acc [] = (reverse acc, [])
+parseTagSubsequentBytes acc (x:xs)
+  | (x .&. 0x80) == 0x80 = parseTagSubsequentBytes (x : acc) xs
+  | otherwise            = (reverse (x : acc), xs)
+
+byteAsHex :: Byte -> String
+byteAsHex b = printf "%02s" (showHex b "")
+
+byteAsBinary :: Byte -> String
+byteAsBinary b = printf "%08s" (showIntAtBase 2 intToDigit b "")
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,5 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
-import Data.Word
-
 import Test.Hspec
 
 import TLV
@@ -9,7 +7,20 @@ import TLV
 main :: IO ()
 main = hspec $ do
   describe "parseTag" $ do
-    it "can parse a tag from an array" $ do
-      let input :: [Word8] = [0x9F, 0x34, 0x01, 0x01]
-      let expected :: ([Word8], [Word8]) = ([0x9F, 0x34], [0x01, 0x01])
+    it "can parse a 1-byte long tag" $ do
+      let input = [0x57, 0x01, 0x01]
+          expected = ([0x57], [0x01, 0x01])
       parseTag input `shouldBe` expected
+
+    it "can parse a 2-byte long tag" $ do
+      let input = [0x9F, 0x34, 0x01, 0x01]
+          expected = ([0x9F, 0x34], [0x01, 0x01])
+      parseTag input `shouldBe` expected
+
+    it "can parse a 3-byte long tag" $ do
+      let input = [0x9F, 0x9F, 0x34, 0x01, 0x01]
+          expected = ([0x9F, 0x9F, 0x34], [0x01, 0x01])
+      parseTag input `shouldBe` expected
+
+    it "gracefully handles no bytes" $ do
+      parseTag [] `shouldBe` ([], [])
